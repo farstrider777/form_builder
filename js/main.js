d0261825bebc8bb0c558515011612de372673bfc
Joshua Bradford <jjyet@yahoo.com>
2017-02-14 21:33:24 -0500

    styled boxes, redid classes with info from api as the selector instead

--- js/main.js
+++ js/main.js
@@ -15,13 +15,16 @@ function printTextBox(data, status, xhrObject){
   for(var count = data.length - 1; count >= 0; count--){
     //debugger
     if(count === 4){
-      $(".header").after(`<input class="input" placeholder="${data[count].label}"
-      type="text"><i class="fa ${data[count].icon}" aria-hidden="true"></i><select class="select">`)
-      for(var i = 0; i < data[4].options.length; i++){
-       $("select").append(`<option value="${data[4].options[i].label}">${data[4].options[i].label}</option>`)
+      $(".header").after(`<input type="${data[count].type}" placeholder="${data[count].label}..."
+      type="text"><i class="fa ${data[count].icon}" aria-hidden="true"></i><select>`)
+      for(var i = 0; i < data[count].options.length; i++){
+       $("select").append(`<option value="${data[count].options[i].label}">${data[count].options[i].label}</option>`)
       }
-    }else{
-      $(".header").after(`<div class="i-container"><input class="input" placeholder="${data[count].label}"
+    }
+    //else if(1 = 0){}
+
+    else{
+      $(".header").after(`<div class="i-container"><input type="${data[count].type}" placeholder="${data[count].label}"
       type="text"><i class="fa ${data[count].icon}" aria-hidden="true"></i></div>`);
     }
   }
